fe7fd47937b3966a52d59823fd47916fa8eed9d1
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-04-16 22:25:48 -0300

    update files

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,50 @@
+package me.dio.academia.digital.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "td_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+
+public class Aluno {
+
+  @Id
+  @GeneratedValue
+
+  private Long id;
+
+  private String nome;
+
+  @Column(unique = true)
+  private String cpf;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+
+}
